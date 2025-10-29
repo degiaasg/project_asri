0ee0c455656cda119373851b8467c82a5f4c90e2
Degia ASG <121994198+degiaasg@users.noreply.github.com>
2025-10-29 15:25:09 +0700

    Kita bermain dengan Data Analyst dengan menggunakan VS Code. This Phase is Done

---
+++ dataset.py
@@ -0,0 +1,10 @@
+import Pandas as pd
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'David'],
+    'Age': [24, 30, 22, 35],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']
+}
+
+df = pd.DataFrame(data)
+print(df)
